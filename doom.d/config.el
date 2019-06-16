5e46f7d43b47fc2325f3af6c8951d5d1b74d7870
emil-r-petersen <emilrosendahlpetersen@outlook.com>
2019-06-16 17:12:07 -0400

    workspace keymap

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,12 +21,33 @@
 (show-paren-mode 1)
 (display-time-mode 1)
 
-(setq org-imenu-depth 5)
-
 (after! org
-  (load-file "~/capture-templates.el"))
+  (load-file "~/capture-templates.el")
+  (setq org-imenu-depth 15)
+  (setq imenu-max-items 0))
 
 ;; Set the dvorak version of the improve home row bindings
 (setq aw-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n))
 
-(map! (:map override) :g "C-c w" '+workspace/switch-to)
+(defvar my-workspace-map (make-sparse-keymap)
+  "Project and workspace shifting")
+
+(map! (:map my-workspace-map
+        "." #'+workspace/switch-to
+        "n" #'+workspace/new
+        "s" #'+workspace/save
+        "l" #'+workspace/load
+        "r" #'+workspace/rename
+        "o" #'+workspace/other
+        [tab]  #'+workspace/display
+        "d"  #'+workspace/delete
+        "1" #'+workspace/switch-to-0
+        "2" #'+workspace/switch-to-1
+        "3" #'+workspace/switch-to-2
+        "4" #'+workspace/switch-to-3
+        "5" #'+workspace/switch-to-4
+        "6" #'+workspace/switch-to-5
+        "7" #'+workspace/switch-to-6
+        "8" #'+workspace/switch-to-7
+        "9" #'+workspace/switch-to-8)
+      :g "C-c w" my-workspace-map)
